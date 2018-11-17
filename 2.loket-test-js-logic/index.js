ca2949bdb29e4b8a1557ed6816fad2833358705f
Fathan Rohman <fathan@ummi.ac.id>
2018-11-17 13:43:36 +0700

    init form js logic

--- 2.loket-test-js-logic/index.js
+++ 2.loket-test-js-logic/index.js
@@ -1,3 +1,12 @@
 function handleClick () {
-  alert('Hello World')
+  var valName = document.getElementById('Name').value;
+  var valEmail = document.getElementById('Email').value;
+  var valPhoneCode = document.getElementById('PhoneCode').value;
+  var valPhone = document.getElementById('Phone').value;
+  var valAddress = document.getElementById('Address').value;
+  var valSubscribe = document.getElementById('Subscribe').value;
+
+  var btnSubmit = document.getElementById('Submit');
+
+  console.log(valName)
 }
